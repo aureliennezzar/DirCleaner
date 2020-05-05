306be7cb1ca243fe01f3ce1a87d8d5d4206b6772
Aurelien NEZZAR <aurelien.nezzar@outlook.fr>
2020-05-05 14:02:18 +0200

    DirCleaner v2.0.0

--- aboutUs.py
+++ aboutUs.py
@@ -8,4 +8,4 @@ def menu_aboutus():
 		linesplit=configlines[line].split("<dcv2>")
 		extadded=linesplit[0];storfold = linesplit[1].replace("\n","")
 		aboutlist+= "{0}  >>>   {1}\n".format(extadded, storfold.split("/")[-1])
-	tkinter.messagebox.showinfo('About Us','Version 0.7\n\nBy: Jin\nStorage folder : {0}\nExtensions/Folder : \n{1}'.format(cleanfold,aboutlist))
+	tkinter.messagebox.showinfo('About DCleaner','Version: 2.0.0 / Author: Jin\n\n----Your storage folder location---- \n\n>>>> {0}\n----Your extensions/folders saved ----\n\n{1}'.format(cleanfold,aboutlist))
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/bin/python
 # -*- coding: utf-8 -*-
-#Name :
-#Author :
-#Date :
+#Name : DirCleaner V2
+#Author : strak_stark
+#Date : 2020
 
 #=======================================================================
 from tkinter import *
@@ -14,19 +14,18 @@ from tkinter.messagebox import askokcancel
 import os
 from os import path
 import shutil
-from aboutUs import menu_aboutus
+
 
 global btn1,btn2,btn3,cleanfoldB
 root = Tk()
 root.resizable(0, 0)
-
 def createfolder():
 	global e,e2,new_window
 	new_window = Toplevel(root)
 	new_window.attributes("-topmost", 1)
 	new_window.resizable(0, 0)
-	new_window.geometry('200x150')
-	new_window.title("Créer le dossier de TRI")
+	new_window.geometry('250x75')
+	new_window.title("Create storage folder")
 	label1 = Label(new_window, text="Name for your storage folder")
 	label1.pack()
 
@@ -53,8 +52,8 @@ def createfolder2():
 		configfile = open("config.dat","a")
 		configfile.write(foldname);configfile.close()
 		cleanfold=foldname
+		tkinter.messagebox.showinfo('One more step ! ','Now you just have to add the extensions you want to move by pressing "Add extensions",\n then just chose the folder to keep this particular files...\n\nGood Luck, my email if you want to report bugs : email@exemple.com')
 		new_window.destroy()
-
 def addex_add():
 	extension= entryex.get()
 	if extension=="":
@@ -91,16 +90,19 @@ def addex():
 def addexexit():
 	btn1['state']=NORMAL;btn2['state']=NORMAL;btn3['state']=NORMAL;btn4['state']=NORMAL
 	window_addex.destroy()
-
-
 def func_movefolds():
 	btn1['state']=DISABLED;btn2['state']=DISABLED;btn3['state']=DISABLED;btn4['state']=DISABLED
 	fold2mov = filedialog.askdirectory(initialdir="/",title="Wich folder do you want to move ?")
 	btn1['state']=NORMAL;btn2['state']=NORMAL;btn3['state']=NORMAL;btn4['state']=NORMAL
-	shutil.move(fold2mov, cleanfold.replace("\n","/FOLDERS"))
+	if fold2mov=="":
+		return
+	else:
+		shutil.move(fold2mov, cleanfold.replace("\n","/FOLDERS"))	
 	tkinter.messagebox.showinfo('Done!','Folder moved...')
 	return
 def func_clean():
+	global var_chk,choice1,choice2
+	choice1,choice2=False,False
 	btn1['state']=DISABLED;btn2['state']=DISABLED;btn3['state']=DISABLED;btn4['state']=DISABLED
 	extstoclean,storingfolders = [],[]
 	configfile = open("config.dat","r")
@@ -117,6 +119,8 @@ def func_clean():
 	else:
 		dirtoclean+="/"
 	deskfiles = os.listdir(dirtoclean)
+	errnum=0
+	errfiles=[]
 	for ext in range(len(extstoclean)):
 		for f in deskfiles:
 			if extstoclean[ext] in f:
@@ -126,8 +130,41 @@ def func_clean():
 					tkinter.messagebox.showinfo('Done!','{0} Already exists in {1}, it will not be moved...'.format(f, storingfolders[ext]))
 					pass
 
+
 	btn1['state']=NORMAL;btn2['state']=NORMAL;btn3['state']=NORMAL;btn4['state']=NORMAL
 	tkinter.messagebox.showinfo('Done!','Folder cleaned !')
+def window_errf_show(file,choice):
+	global window_errf,var_chk
+	if choice==0:
+		var_chk = IntVar()
+		window_errf = Toplevel(root)
+		window_errf.resizable(0, 0)
+		window_errf.geometry('300x100')
+		window_errf.title("This file already exists ! ")
+		errf_label = Label(window_errf,text="[{}] A file with this name already exists".format(file))
+		errf_label.pack()
+		errf_btn = Button(window_errf,text="Don't move",command=errf1)
+		errf_btn.pack()
+		errf_btn2 = Button(window_errf,text="Move but keep both files",command=errf2)
+		errf_btn2.pack()
+		if errnum>1:
+			errf_apply = Radiobutton(window_errf, text="Apply my choice for the next {} files ".format(errnum), variable=var_chk, value=1)
+			errf_apply.pack()
+	elif choice==1:
+		pass
+	elif choice==2:
+		num = 1
+		while True:
+			try:
+				shutil.move("{}".format(file[0], file[1]))
+			except Exception:
+				errnum+=1
+
+def errf1():
+	global choice1,choice2
+	choice = var_chk.get()
+	if choice:
+		choice1=True
 #Create menubar
 menubar = Menu(root)
 root.config(menu = menubar)
@@ -136,16 +173,13 @@ subMenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="File", menu = subMenu)
 subMenu.add_command(label="Exit", command=exit)
 
-# subMenu = Menu(menubar, tearoff=0)
-# menubar.add_cascade(label="Edit", menu = subMenu)
-# subMenu.add_command(label="Add music", command=add_music)
-
 subMenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Help", menu = subMenu)
-subMenu.add_command(label="About Us", command = menu_aboutus)
+subMenu.add_command(label="About DCleaner", command = menu_aboutus)
 
 root.geometry('250x175')
-root.title("DirCleaner Beta")
+root.title("DirCleaner 2.0.0")
+root.iconbitmap('brush.ico')
 btn1 = Button(root,text='Clean a folder',command=func_clean)
 btn1.pack(expand="yes")
 btn2 = Button(root,text='Add extensions',command=addex)
@@ -159,6 +193,7 @@ btn3.pack(expand="yes")
 
 
 if not path.exists("config.dat"):
+	tkinter.messagebox.showinfo('Welcome!','Welcome to DCleanerV2 ! Its your first time on the program, so you have to enter a name for your storage folder ! \n All files/Folders that you want to move will be moved inside it...\nFollow the step and enjoy ! ')
 	createfolder()
 else:
 	configfile = open("config.dat","r")
